b8f7db5f275c6fd13de775efc5fe44b5bb26d13c
mxdlong88 <383301882@qq.com>
2017-10-11 09:52:05 +0800

    合并

--- webapp/src/js/controller/actcard_controller.js
+++ webapp/src/js/controller/actcard_controller.js
@@ -1,4 +1,4 @@
-angular.module('myApp').controller('actcard_controller', function($scope, $interval){
+angular.module('myApp').controller('actcard_controller', function($scope, $interval,actcard_service){
     var card = {};
     $scope.card = card;
     //所有信息对象
@@ -7,15 +7,49 @@ angular.module('myApp').controller('actcard_controller', function($scope, $inter
     //页签控制
     var pageflag = 0;
     $scope.pageflag = pageflag;
+    //卡片信息
+    var cardtype = [];
+    $scope.cardtype = cardtype;
+    //产品签约信息
+    var productList = [];
+    $scope.productList = productList;
+    //身份证图片
+    var idcardphoto = "";
+    $scope.idcardphoto = idcardphoto;
     //下一步
     card.next = function (pageflag) {
+        console.log(pageflag)
         if(pageflag == 0){
             for(var i in $scope.bar){
                 $scope.bar[i].class = "todo";
             }
             $scope.bar[pageflag].class = "finished";
+            //请求卡片数据
+            var subFrom = {};
+            subFrom.tellerNo = "900001005";
+            subFrom.branchId = "90091";
+            actcard_service.queryCard(subFrom).then(function (data) {
+                $scope.cardtype = data.bsadata;
+                console.log(data);
+            })
         }else{
             $scope.bar[pageflag].class = "finished";
+            if(pageflag == 2){
+
+            }
+            if(pageflag == 3){
+                //查询产品信息
+                var subFrom = {};
+                subFrom.tellerNo = "900001005";
+                subFrom.branchId = "90091";
+                actcard_service.queryProduct(subFrom).then(function (data) {
+                    $scope.productList = data.bsadata;
+                    console.log(data)
+                })
+            }
+            if(pageflag == 6) {
+                console.log($scope.subFrom)
+            }
         }
         pageflag++;
         $scope.pageflag = pageflag;
@@ -86,18 +120,13 @@ angular.module('myApp').controller('actcard_controller', function($scope, $inter
     bar.push(obar7);
 
     /**--------------------------------模拟卡片类型-------------------------------*/
-    var cardtype = [{"cardName":"白金卡","pictureName":"platinum","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资  普通借记卡 投资理财 存取款 代"}]},
-        {"cardName":"金卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资  普通借记卡 投资理财 存取款 代"}]},
-        {"cardName":"黑色卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资   普通借记卡 投资理财 存取款 代"}]},
-        {"cardName":"尊享卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资  普通借记卡 投资理财 存取款 代"}]},
-        {"cardName":"青春卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资   普通借记卡 投资理财 存取款 代"}]},
-        {"cardName":"量贩卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资   普通借记卡 投资理财 存取款 代"}]}
-
-
-
-
-        ];
-    $scope.cardtype = cardtype;
+    // var cardtype = [{"cardName":"白金卡","pictureName":"platinum","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资  普通借记卡 投资理财 存取款 代"}]},
+    //     {"cardName":"金卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资  普通借记卡 投资理财 存取款 代"}]},
+    //     {"cardName":"黑色卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资   普通借记卡 投资理财 存取款 代"}]},
+    //     {"cardName":"尊享卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资  普通借记卡 投资理财 存取款 代"}]},
+    //     {"cardName":"青春卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资   普通借记卡 投资理财 存取款 代"}]},
+    //     {"cardName":"量贩卡","pictureName":"gold","product":[{"des":"上善卡慈善主题 普通借记卡 投资理财 存取款 代发工资   普通借记卡 投资理财 存取款 代"}]}];
+    // $scope.cardtype = cardtype;
     //卡片详情页签控制
     var carddetails = false;
     $scope.carddetails = carddetails;
@@ -119,11 +148,26 @@ angular.module('myApp').controller('actcard_controller', function($scope, $inter
     //成功
     card.idcardsuccess = function () {
         $scope.iddetails = true;
-        var idcard = {};
-        idcard.id = "123123123";
-        idcard.name = "王小二";
-        idcard.date = "0000-00-00";
-        $scope.subFrom.idcard = idcard;
+
+        //身份核查方法,外设激活后调用
+        //外设获取信息
+        var subFrom = {};
+        subFrom.vcherType = "101";
+        subFrom.vcherNo = "522222199007112835";
+        subFrom.name = "杨超";
+        subFrom.phFlag = "1";
+        subFrom.tellerNo = "900001005";
+        subFrom.branchId = "900001";
+        actcard_service.singleCheck(subFrom).then(function (data) {
+            $scope.idcardphoto = data.bsadata.photo;
+            var idcard = {};
+            idcard.vcherType = "101";
+            idcard.vcherNo = "522222199007112835";
+            idcard.name = "杨超";
+            idcard.visBrno = data.bsadata.visBrno;
+            $scope.subFrom.idcard = idcard;
+            console.log(data.bsadata)
+        })
     }
     //失败
     card.idcarderror = function () {
@@ -135,14 +179,14 @@ angular.module('myApp').controller('actcard_controller', function($scope, $inter
 
 
     /**----------------------------------模拟产品签约--------------------------------*/
-    var productList = [
-        {"productName":"手机银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"可指纹登录","optionValue":"01"},{"optionName":"支付限额","optionValue":"02"}]},
-        {"productName":"网上银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
-        {"productName":"短信通","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
-        {"productName":"电话银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
-        {"productName":"电话银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
-                        ];
-    $scope.productList = productList;
+    // var productList = [
+    //     {"productName":"手机银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"可指纹登录","optionValue":"01"},{"optionName":"支付限额","optionValue":"02"}]},
+    //     {"productName":"网上银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
+    //     {"productName":"短信通","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
+    //     {"productName":"电话银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
+    //     {"productName":"电话银行","option":[{"optionName":"开通","optionValue":"00"},{"optionName":"需要动态口令","optionValue":"01"},{"optionName":"转账限额","optionValue":"02"}]},
+    //                     ];
+    // $scope.productList = productList;
     
     //确认签约
     card.sign = function (pageflag) {
