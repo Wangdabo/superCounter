35d30e3522c5f5540a98542344190a9971d8b0d2
Wangdabo <897653368@qq.com>
2017-10-26 15:44:12 +0800

    图片上传更改
    图片上传更改

--- webapp/src/js/controller/actcard_controller.js
+++ webapp/src/js/controller/actcard_controller.js
@@ -72,7 +72,7 @@ angular.module('myApp').controller('actcard_controller', function($scope, $inter
                 var COH = {};
                 var CTL = {};
                 BDY = $scope.subFrom;
-                var now = new Date();
+                var now = new Date().Format("yyyy-MM-dd hh:mm:ss");
                 //COH
                 COH.TRANSDATE = now;
                 COH.TRANSTELLER = "900001001";
@@ -91,7 +91,7 @@ angular.module('myApp').controller('actcard_controller', function($scope, $inter
                 COH.consumerId = "301";
                 COH.TELLERWORKMODEL = "6";
                 COH.TRANSSERIALNO = $scope.transSerialNo;
-                COH.TRANSINSTNO = "90001";
+                COH.TRANSINSTNO = "CN0019009";
                 COH.TRANSTIME = now;
                 COH.TRANSAUTHNO = "";
                 //CTL
@@ -332,5 +332,21 @@ angular.module('myApp').controller('actcard_controller', function($scope, $inter
         // $scope.subFrom.selectProduct = testselsr;
         card.next(pageflag);
 
+    }
+
+    Date.prototype.Format = function (fmt) { //author: meizz
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
     }
 })
